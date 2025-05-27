cd2515b1bb51b8e47b9f90e45ada479c882e2995
Jorge Antonio Moreno <antoniomoreno.contacto@gmail.com>
2025-05-26 20:21:20 -0600

    Proyecto inicializado y Tabla de candidatos creada

---
+++ backend/prisma/migrations/20250527021951_add_candidates_table/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Candidate" (
+    "id" SERIAL NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "education" TEXT,
+    "work_experience" TEXT,
+    "resume_file" BYTEA,
+    "resume_file_type" VARCHAR(5),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Candidate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Candidate_email_key" ON "Candidate"("email");
